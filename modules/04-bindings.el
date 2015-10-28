64f70051152cb3dac94fc46b8c5181faa96d7d93
Greg Leppert <greg@leppert.me>
2015-10-28 10:34:07 -0400

    Disable swipe

--- modules/04-bindings.el
+++ modules/04-bindings.el
@@ -72,9 +72,9 @@
 
 ;; Use swiper in place of isearch
 ;; https://github.com/abo-abo/swiper#installation
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-(global-set-key "\C-s" 'swiper)
-(global-set-key "\C-r" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key [f6] 'ivy-resume)
+;; (ivy-mode 1)
+;; (setq ivy-use-virtual-buffers t)
+;; (global-set-key "\C-s" 'swiper)
+;; (global-set-key "\C-r" 'swiper)
+;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; (global-set-key [f6] 'ivy-resume)
