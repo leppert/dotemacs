13a4be8e6fe894614701d50f70db4a60fc08c04d
Greg Leppert <greg@leppert.me>
2015-10-29 10:15:54 -0400

    Add window resizing bindings

--- modules/04-bindings.el
+++ modules/04-bindings.el
@@ -78,3 +78,10 @@
 ;; (global-set-key "\C-r" 'swiper)
 ;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
 ;; (global-set-key [f6] 'ivy-resume)
+
+;; Window resizing
+;; via: http://emacswiki.org/emacs/WindowResize
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
